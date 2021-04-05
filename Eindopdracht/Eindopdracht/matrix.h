6bf7ad435e6db03f5ea355cdaee693276a3f8c2a
Jurre Vlas <j.vlas@student.avans.nl>
2021-04-05 17:39:22 +0200

    matrix.c and matrix.h added

---
+++ Eindopdracht/Eindopdracht/matrix.h
@@ -0,0 +1,15 @@
+
+#ifndef MATRIX_H_
+#define MATRIX_H_
+
+void twi_init(void);
+void twi_start(void);
+void twi_stop(void);
+void twi_tx(unsigned char data);
+void wait( int ms );
+void start_init(void);
+void decision(int good);
+
+
+
+#endif /* MATRIX_H_ */
